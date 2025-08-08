ccf1459df7a0352de7024fb506994ea0c3f65c0e
Laysalbach <lays.albach@escola.pr.gov.br>
2025-08-08 12:05:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+ccriaCartao(
+    'Historia', 
+    'Em que ano o Brasil se tornou independente?', 
+    '1822'  
+ )
+ 
+ 
+ criaCartao(
+    'Artes', 
+    'Quem pintou o quadro Noite Estrelada', 
+    'Vincent Van Gogh' 
+ )
+ 
+ 
+ criaCartao(
+    'Matemática', 
+    'O que é um poligono?', 
+    'Figuras geométricas plana fechada ' 
+ )
+ 
+ 
+ criaCartao(
+    'Lingua portuguesa', 
+    'Qual é a estrutura de um poema?', 
+    'Estrofes, versos, podendo apresentar rimas' 
+ )
+ 
+ 
+ 
+ 
